6bcf9fba767e3f90932ced26d15346d7a57bcf42
khan zaid <khanzaid786786.kz@gmail.com>
2021-04-02 22:05:59 +0530

    Updates

---
+++ precache-manifest.4d2e274e3615d0fbb7b935b4f2c07248.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "88b3351cadab823ba8ff",
+    "url": "/bringmakes/static/js/main.f0ff8f55.chunk.js"
+  },
+  {
+    "revision": "a97d2a9c3eb983d2f78f",
+    "url": "/bringmakes/static/js/runtime~main.3addacdb.js"
+  },
+  {
+    "revision": "8fc52b7205f534fb12fb",
+    "url": "/bringmakes/static/js/2.e1a64077.chunk.js"
+  },
+  {
+    "revision": "60b32d54e3861a1abd65",
+    "url": "/bringmakes/static/js/3.7a7183d0.chunk.js"
+  },
+  {
+    "revision": "6ce25d1efe3e06a22d8d",
+    "url": "/bringmakes/static/js/4.876195f4.chunk.js"
+  },
+  {
+    "revision": "9e73c523efa84c44306b",
+    "url": "/bringmakes/static/js/5.2a2d5ff2.chunk.js"
+  },
+  {
+    "revision": "12bf012a0d18878a499a",
+    "url": "/bringmakes/static/js/6.69a83be9.chunk.js"
+  },
+  {
+    "revision": "4d4baf0385d5f439217445e56e0bab90",
+    "url": "/bringmakes/static/media/newImg1.4d4baf03.png"
+  },
+  {
+    "revision": "a677a544374bfdb769264981a6ddb030",
+    "url": "/bringmakes/static/media/workIcon1.a677a544.svg"
+  },
+  {
+    "revision": "f8071f5fdc5bef5f16b4b3cf75ef7837",
+    "url": "/bringmakes/static/media/workIcon2.f8071f5f.svg"
+  },
+  {
+    "revision": "4a095c9310df12d098d5ee2bd9efdf8e",
+    "url": "/bringmakes/static/media/workIcon3.4a095c93.svg"
+  },
+  {
+    "revision": "a7649c0ce7aadf2835e59e6b56b61d9a",
+    "url": "/bringmakes/static/media/workIcon4.a7649c0c.svg"
+  },
+  {
+    "revision": "4bd4cd54a0e58c6e5e8406638ad1a462",
+    "url": "/bringmakes/static/media/workIcon5.4bd4cd54.svg"
+  },
+  {
+    "revision": "da54f269962c261aca68d41af7499330",
+    "url": "/bringmakes/static/media/heroBgImg.da54f269.png"
+  },
+  {
+    "revision": "2b976b3bc2a79bfe0e0f8c1163b4c20a",
+    "url": "/bringmakes/static/media/heroMobileImg.2b976b3b.png"
+  },
+  {
+    "revision": "2bf77bab22ad5c15b545d4bad8139dd6",
+    "url": "/bringmakes/static/media/featuresImg1.2bf77bab.png"
+  },
+  {
+    "revision": "c61faadbe462657426a9df5b6a002b07",
+    "url": "/bringmakes/static/media/featuresImg2.c61faadb.png"
+  },
+  {
+    "revision": "950d275e5174af0673b132cb1e759ccc",
+    "url": "/bringmakes/static/media/featuresImg3.950d275e.png"
+  },
+  {
+    "revision": "d556098f00395f3a8baef8a752452ffd",
+    "url": "/bringmakes/static/media/featuresImg4.d556098f.png"
+  },
+  {
+    "revision": "a70140f68de092890adb07b5ebd9fd63",
+    "url": "/bringmakes/static/media/tastyFoodImg.a70140f6.png"
+  },
+  {
+    "revision": "c74d77dfedda071deb53670e9e06e152",
+    "url": "/bringmakes/static/media/workPhoneImg.c74d77df.png"
+  },
+  {
+    "revision": "b6b20f9b0f15ac314b4cd5bd64782a32",
+    "url": "/bringmakes/static/media/workGreenIcon1.b6b20f9b.svg"
+  },
+  {
+    "revision": "6795f563d14d45299c65f4538a487286",
+    "url": "/bringmakes/static/media/workGreenIcon2.6795f563.svg"
+  },
+  {
+    "revision": "b83be9c63326e5472d19e48b71c4726b",
+    "url": "/bringmakes/static/media/workGreenIcon3.b83be9c6.svg"
+  },
+  {
+    "revision": "2347fb90302ec381b635b4d561bd5e1b",
+    "url": "/bringmakes/static/media/workGreenIcon4.2347fb90.svg"
+  },
+  {
+    "revision": "b213fc1a795d11e1469966af75d495df",
+    "url": "/bringmakes/static/media/workGreenIcon5.b213fc1a.svg"
+  },
+  {
+    "revision": "2cb7189c8e4fe5eb58d5f263efec284b",
+    "url": "/bringmakes/static/media/getAppBgImg.2cb7189c.svg"
+  },
+  {
+    "revision": "f3e462af11853b7bdb31b797ed61545c",
+    "url": "/bringmakes/static/media/googlePlay.f3e462af.png"
+  },
+  {
+    "revision": "0bd5f566611b276a56a23065bafbd29d",
+    "url": "/bringmakes/static/media/appleStore.0bd5f566.png"
+  },
+  {
+    "revision": "06675d08e6dd4f1ef38bf15854f8aa55",
+    "url": "/bringmakes/static/media/aboutUs.06675d08.png"
+  },
+  {
+    "revision": "f7caa7204093bd74480502fa38715c78",
+    "url": "/bringmakes/static/media/reasonsImg1.f7caa720.svg"
+  },
+  {
+    "revision": "faeb10d9bf96c997710388ccdba5c5d9",
+    "url": "/bringmakes/static/media/reasonsImg2.faeb10d9.svg"
+  },
+  {
+    "revision": "c3fc357f48b34e74fe0514b9efb7cdf1",
+    "url": "/bringmakes/static/media/reasonsImg3.c3fc357f.svg"
+  },
+  {
+    "revision": "79ea92471b4a53b3a01637c6d732638e",
+    "url": "/bringmakes/static/media/reasonsImg4.79ea9247.svg"
+  },
+  {
+    "revision": "ec71ece7e5f35ea892f5528dcc3245d8",
+    "url": "/bringmakes/index.html"
+  }
+];
